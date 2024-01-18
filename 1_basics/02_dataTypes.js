0fea7639a52485fe577f9bf5084e2da71a3b32cc
Aryan Singh <40730754+aryanonweb@users.noreply.github.com>
2024-01-18 07:48:21 +0000

    dataTypes

---
+++ 1_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+// number => 2 to power 53
+// bigint 
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+//object 
+
+console.log(typeof undefined); //undefined coz it is type in js
+console.log(typeof null); // object 
