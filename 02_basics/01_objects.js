0142ca40d9a516e40c700aca733ff65c178bbacd
Aryan Singh <40730754+aryanonweb@users.noreply.github.com>
2024-03-04 13:13:13 +0000

    objects discuss

---
+++ 02_basics/01_objects.js
@@ -0,0 +1,33 @@
+// const tinderUser =new Object() //singleton object 
+
+const tinderUser={} //non singleton object
+
+tinderUser.id ="123abc"
+
+tinderUser.name="Aryan"
+tinderUser.isLoogedIn=false
+
+// console.log(tinderUser);
+
+const regularUser= {
+    email: "sum@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Aryan",
+            lastname: "Singh"
+        }
+    }
+}
+// console.log(regularUser.fullname)
+
+const obj1 = {1: "a",2:"b"}
+const obj2= {3:"a",4:"b"}
+
+// const obj3= Object.assign({},obj1, obj2)
+// const obj3={...obj1,...obj2}
+// console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoogedIn'));
