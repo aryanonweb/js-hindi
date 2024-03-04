fe79ceb21631e8dbc9e6652c0ab8c384ef9f9514
Aryan Singh <40730754+aryanonweb@users.noreply.github.com>
2024-03-04 13:29:45 +0000

    objects basics

--- 02_basics/01_objects.js
+++ 02_basics/01_objects.js
@@ -27,7 +27,29 @@ const obj2= {3:"a",4:"b"}
 // const obj3={...obj1,...obj2}
 // console.log(obj3);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('isLoogedIn'));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoogedIn'));
+
+
+//destructure of objects
+const course= {
+    coursename: "js in Hindi",
+    price: "999",
+    courseInstructor: "Aryan"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+//API 
+
+{
+    name: "Aryan ",
+    coursename: "js in hindi",
+    price: "free"
+}
